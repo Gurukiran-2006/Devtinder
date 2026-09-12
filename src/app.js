f795de9f0fb82721b21f05367f1a9e1be4813433
Gurukiran-2006 <gurukiranmgowda20@gmail.com>
2026-09-12 10:23:18 +0530

    exploring the express routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,35 @@
 const express=require("express");
 const app=express();
 
+
+
 app.use("/text",(req,res)=>{
     res.send("hello from the dashboard gk tinder");
 })
-app.use((req,res)=>{
+app.use("/server",(req,res)=>{
     res.send("hello from the srever");
 });
+
+app.use("/user",(req,res)=>{
+    res.send("hey hi how are you ?");
+});
+
+app.get("/user",(req,res)=>{
+res.send({firstname : "gk",
+          lastname:"gowda"});
+});
+
+app.post("/user",(req,res)=>{
+res.send("data us saved successfully in the database")
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("deleted user successfully");
+})
+
+
 
 app.listen(7777,()=>{
     console.log("server is listening on port 7777....")
 });
+
